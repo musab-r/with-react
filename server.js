2f20d5254d7ed0708e1ae26b4c08f0196fcab26e
Musab Rasheed <musab.rasheed@u-blox.com>
2022-09-13 23:22:36 +0500

    express learning

--- server.js
+++ server.js
@@ -17,17 +17,17 @@
 // });
 
 // Section 2.3
-import http from 'http';
+// import http from 'http';
 
-const server = http.createServer((req, res) => {
-    res.write('Hello HTTP\n');  
-    setTimeout(() =>{
-        res.write('I can stream!!\n');
-        res.end();
-    }, 3000);
-});
+// const server = http.createServer((req, res) => {
+//     res.write('Hello HTTP\n');  
+//     setTimeout(() =>{
+//         res.write('I can stream!!\n');
+//         res.end();
+//     }, 3000);
+// });
 
-server.listen(8080);
+// server.listen(8080);
 
 // emitted event
 // server.on('request', (req, res) => {
@@ -37,6 +37,32 @@ server.listen(8080);
 //         res.end();
 //     }, 3000);
 // });
+
+// Section 2.4 Express
+import express from 'express';
+import config from './config';
+import fs from 'fs';
+
+const server = express();
+
+// routing 
+server.get('/', (req, res)=> {
+    res.send("Hello Express!");
+})
+
+// 1st way
+server.use(express.static('public'));
+// 2nd way
+// server.get('/about.html', (req, res) => {
+//     fs.readFile('./about.html', (err, data) => {
+//         res.send(data.toString());
+//     })
+    
+// });
+
+server.listen(config.port, ()=> {
+    console.info('Express is listening to the port ', config.port);
+});
 
 
 
